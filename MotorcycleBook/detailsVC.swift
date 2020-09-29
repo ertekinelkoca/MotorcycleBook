008fb77689d809b1032776c5b5c92026322deef0
Ali Ertekin Elkoca <30657493+ertekinelkoca@users.noreply.github.com>
2020-09-29 16:01:22 +0300

    segue and variables created

---
+++ MotorcycleBook/detailsVC.swift
@@ -0,0 +1,25 @@
+//
+//  detailsVC.swift
+//  MotorcycleBook
+//
+//  Created by mac on 29.09.2020.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
