2dd5ec6c404fe1f958c45ed81e33c6d6bf758848
Ali Ertekin Elkoca <30657493+ertekinelkoca@users.noreply.github.com>
2020-09-30 01:21:11 +0300

    model and segue created

--- MotorcycleBook/ViewController.swift
+++ MotorcycleBook/ViewController.swift
@@ -10,21 +10,40 @@ import UIKit
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    var motorArray = [Motorcycle]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
+        
+        //Motorcycle Objects
+        let bmw = Motorcycle(modelName: "S1000RR", modelDescription: "HP4", modelView: UIImage(named: "HP4")!)
+       
+        let kawasaki = Motorcycle(modelName: "ZX10R", modelDescription: "RR", modelView: UIImage(named: "zx10r")!)
+        
+        let yamaha = Motorcycle(modelName: "R1", modelDescription: "M Carbon", modelView: UIImage(named: "yzfr1")!)
+        
+        let honda = Motorcycle(modelName: "CBR1000RR", modelDescription: "FIREBLADE SP", modelView: UIImage(named: "cbr1000rrsp")!)
+        
+        let suzuki = Motorcycle(modelName: "GSXR-1300", modelDescription: "HAYABUSA", modelView: UIImage(named: "gsxr1000")!)
+        
+        motorArray.append(bmw)
+        motorArray.append(kawasaki)
+        motorArray.append(yamaha)
+        motorArray.append(honda)
+        motorArray.append(suzuki)
+    
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return motorArray.count-1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "racing"
+        cell.textLabel?.text = motorArray[indexPath.row].name
         return cell
     }
 
---
+++ MotorcycleBook/motorcycle.swift
@@ -0,0 +1,25 @@
+//
+//  motorcycle.swift
+//  MotorcycleBook
+//
+//  Created by mac on 29.09.2020.
+//
+
+import Foundation
+import UIKit
+
+class Motorcycle {
+    
+    var name : String
+    var description : String
+    var image : UIImage
+    
+    
+    init(modelName : String , modelDescription : String , modelView : UIImage) {
+        name = modelName
+        description = modelDescription
+        image = modelView
+    }
+    
+    
+}
